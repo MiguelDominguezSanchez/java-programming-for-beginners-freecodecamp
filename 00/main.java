befa678f41b42fbc6dcf367bc30a9758dfcdf3ec
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-12-17 13:36:54 +0100

    For Loop

--- 00/main.java
+++ 00/main.java
@@ -198,3 +198,61 @@ class Main {
   }
 }
  */
+
+/*
+For loops
+
+class Main {
+  public static void main(String[] args) {
+    // int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+
+    // int sum = 0;
+
+    // int number = 5;
+    
+    // for(int index = 0; index < numbers.length; index++) {
+      // System.out.println(numbers[index]);
+    // }
+
+    // System.out.println(sum);
+
+    // for(int multiplier = 1; multiplier < 10; multiplier++) {
+      // System.out.printf("%d X %d = %d \n", number, multiplier, number * multiplier);
+    // }
+
+    //
+
+    for (int number = 1; number < 10; number++) {
+      for (int multiplier = 1; multiplier <= 10; multiplier++) {
+        System.out.printf("%d X %d = %d \n", number, multiplier, number * multiplier);
+      }
+    }
+  }
+}
+*/ 
+
+/*
+For Loop
+class Main {
+  public static void main(String[] args) {
+    int numbers[] = {1, 2, 3, 4, 5};
+    
+   // for(int number = 1; number <= 50; number++) {
+      // if(number % 2 == 1) {
+        // System.out.println(number);
+      // }
+    // }
+
+    int sum = 0;
+
+    for(int number : numbers) {
+
+      sum += number;
+      
+     //  System.out.println(number);
+    }
+
+    System.out.println(sum);
+  }
+}
+ */
